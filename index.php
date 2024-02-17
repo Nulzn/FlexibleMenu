0d116f2a3e25d175918aa892696da7e7083b5253
Nulzn <68256766+Nulzn@users.noreply.github.com>
2024-02-17 12:46:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./styles/index.css">
+</head>
+<body>
+
+<div class="container">
+    <a href="nrins.php" class="box box-1" data-type="NUCLEAR REACTOR" style="--img: url(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.tbat.co.uk%2Fwp-content%2Fuploads%2F2017%2F12%2Freactor--600x600.jpg&f=1&nofb=1&ipt=e83c00482c0fecffaf4acbcc49d0a4eeae7de64a74f49b8958f87ce84d8548b5&ipo=images)"></a>
+    <a class="box box-2" data-type="IRON MAN" style="--img: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwallpapercave.com%2Fwp%2Fwp2638433.jpg&f=1&nofb=1&ipt=1901dbba88908e8407948014661372cc018d160e91a2395fd4608b04381b649e&ipo=images;)"></a>
+    <a class="box box-3" data-type="FORRE GIBB" style="--img: url(https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fgmedia.playstation.com%2Fis%2Fimage%2FSIEPDC%2Ffortnite-packshot-01-en-28jan22%3F%24native%24&f=1&nofb=1&ipt=da50c5e76c6d658af2e356b211d4f12ecc4d686e23c21d17a49cfd4ea50d7889&ipo=images;)"></a>
+</div>
+
+<?php
+
+?>
+
+</body>
+</html>
---
+++ nrins.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="./styles/nrins.css">
+</head>
+<body>
+
+<div class="container">
+    <h1>Nuclear Reactor Instructions</h1>
+    <div class="image" style="--img: url(https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.tbat.co.uk%2Fwp-content%2Fuploads%2F2017%2F12%2Freactor--600x600.jpg&f=1&nofb=1&ipt=e83c00482c0fecffaf4acbcc49d0a4eeae7de64a74f49b8958f87ce84d8548b5&ipo=images);"></div>
+    <p class="desc">
+        Nuclear reactors are the heart of a nuclear power plant.
+
+        They contain and control nuclear chain reactions that produce heat through a physical process called fission. That heat is used to make steam that spins a turbine to create electricity.
+
+        With more than 400 commercial reactors worldwide
+
+        , including 93 in the United States, nuclear power continues to be one of the largest sources of reliable carbon-free electricity available.
+        Nuclear Fission Creates Heat
+        <br>
+        <br>
+        The main job of a reactor is to house and control nuclear fission—a process where atoms split and release energy.
+        Fission and Fusion: <b>What is the Difference?</b>
+        <br>
+        <br>
+            
+
+        Reactors use uranium for nuclear fuel. The uranium is processed into small ceramic pellets and stacked together into sealed metal tubes called fuel rods. Typically, more than 200 of these rods are bundled together to form a fuel assembly. A reactor core is typically made up of a couple hundred assemblies, depending on power level. 
+
+        Inside the reactor vessel, the fuel rods are immersed in water which acts as both a coolant and moderator. The moderator helps slow down the neutrons produced by fission to sustain the chain reaction.
+
+        Control rods can then be inserted into the reactor core to reduce the reaction rate or withdrawn to increase it.
+
+        The heat created by fission turns the water into steam, which spins a turbine to produce carbon-free electricity.
+    </p>
+</div>
+
+<?php
+
+?>
+
+</body>
+</html>
